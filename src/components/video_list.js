ae8d4502e145a518945035cfc48de50e44b08a9c
David.Rz.Liu <mania7539@gmail.com>
2017-11-28 16:50:07 +0800

    create a list with map

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,14 +1,21 @@
 import React from "react";
+import VideoListItem from "./video_list_item";
 
 const VideoList = (props) => {
     /* The 'props' in the functional component is an argument */
-    const videos = props.videos;
+    // const videos = props.videos;
+    const videoItems = props.videos.map((video) => {
+        return <VideoListItem video={video} />
+    });
+    /* Create the layout result first, and then assign it to a const to make it functional */
 
     return (
         <ul className="col-md-4 list-group">
-            {props.videos.length}
+            {videoItems}
         </ul>
     );
+    // recommend to stay away from for loop as much as possible to avoid typo and mistakes
+    // instead try to using built-in iterator, like the 'map' iterator
 };
 
 export default VideoList;
