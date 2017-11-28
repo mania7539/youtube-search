bf5e09b29e95b9cfd45c8700eeaf5da4c7b4e60f
David.Rz.Liu <mania7539@gmail.com>
2017-11-28 16:15:55 +0800

    refactor functional component into class-based component, also use an ES6 syntax to represent key value pair in one word

--- src/index.js
+++ src/index.js
@@ -1,26 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import SearchBar from "./components/search_bar";
 import YTSearch from "youtube-api-search";
 
 const API_KEY = "AIzaSyDfhiV7sDt5xpP9sjxmJN6ZWNyEE7oW4hI";
 
-/* Youtube Search: pass an JSON object with a search term and the API key */
-/* Will get 5 youtube search results, including thumbnails, title, description, on console log */
-YTSearch({key: API_KEY, term: "surfboards"}, function(data) {
-    console.log(data);
-
-});
-
-
 // Create a new component. 
 // This component will produce some HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            videos: [],
+        }
+        /* A list of videos, so use an array is very reasonable */
+
+        YTSearch({ key: API_KEY, term: "surfboards" }, (videos) => {
+            console.log(videos);
+            this.setState({ videos });
+            // in ES6, the above code is equal to 
+            // this.setState({ videos: videos });
+        });
+        /* Youtube Search: pass an JSON object with a search term and the API key */
+        /* Will get 5 youtube search results, including thumbnails, title, description, on console log */
+        /* The 2nd argument is the callback function */
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
