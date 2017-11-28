8e7a841092349aaf3b6fb3937ae51cfb5485202e
David.Rz.Liu <mania7539@gmail.com>
2017-11-28 17:51:12 +0800

    video list items, got from YTSearch

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,7 +1,31 @@
 import React from "react";
 
-const VideoListItem = (props) => {
-    return <li>Video</li>
+// const VideoListItem = (props) => {
+//     const video = props.video;
+
+//     return <li>Video</li>
+// };
+// The props argument and the first statement together is equal to below ES6 syntax
+// 
+/* The props argument with JSON object format, 
+* tells that there's a 'video' property from props which is got from the argument
+*/
+const VideoListItem = ({video}) => {
+    const imageUrl = video.snippet.thumbnails.default.url;
+
+    return (
+        <li className="list-group-item">
+            <div className="video-list media">
+                <div className="media-left">
+                    <img className="media-object" src={imageUrl} />
+                </div>
+
+                <div className="media-body">
+                    <div className="media-heading">{video.snippet.title}</div>
+                </div>
+            </div>
+        </li>
+    );
 };
 
 export default VideoListItem;
